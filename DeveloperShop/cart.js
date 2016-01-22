ac676dba762738930742ff3e592077c318997693
mfarias <marcela.sfarias@gmail.com>
2016-01-21 23:42:58 -0200

    Estrutura do projeto

---
+++ DeveloperShop/cart.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module('shopCart', []);
+
+app.controller('FormCtrl', function ($scope, $http) {
+
+    $scope.Developers = []
+
+    var formData = {
+        username: "",
+        price: ""
+    };
+
+    $scope.submitForm = function () {
+        formData = $scope.form;
+        $scope.Developers.push({
+            "Username": $scope.username,
+            "Price": $scope.price,
+            "Hours": $scope.hours,
+            "TotalPrice": $scope.price * $scope.hours
+        });
+    };
+
+    $scope.getCartTotalPrice = function () {
+        var total = 0;
+        for (var i = 0; i < $scope.Developers.length; i++) {
+            var dev = $scope.Developers[i];
+            total += dev.TotalPrice;
+        }
+        return total;
+    };
+
+});
---
+++ DeveloperShopAPI.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeveloperShopAPI;
+using DeveloperShopAPI.Controllers;
+using DeveloperShopAPI.Models;
+
+namespace DeveloperShopAPI.Tests.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            ShopController controller = new ShopController();
+
+            // Act
+            ShopCart result = controller.GetShopCart();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Developers.Count());
+            Assert.AreEqual("mfarias", result.Developers.ElementAt(0).Username);
+        }
+
+        //[TestMethod]
+        //public void GetById()
+        //{
+        //    // Arrange
+        //    ShopController controller = new ShopController();
+
+        //    // Act
+        //    string result = controller.Get(5);
+
+        //    // Assert
+        //    Assert.AreEqual("value", result);
+        //}
+
+        //[TestMethod]
+        //public void Post()
+        //{
+        //    // Arrange
+        //    ShopController controller = new ShopController();
+
+        //    // Act
+        //    controller.Post("value");
+
+        //    // Assert
+        //}
+
+        //[TestMethod]
+        //public void Put()
+        //{
+        //    // Arrange
+        //    ShopController controller = new ShopController();
+
+        //    // Act
+        //    controller.Put(5, "value");
+
+        //    // Assert
+        //}
+
+        //[TestMethod]
+        //public void Delete()
+        //{
+        //    // Arrange
+        //    ShopController controller = new ShopController();
+
+        //    // Act
+        //    controller.Delete(5);
+
+        //    // Assert
+        //}
+    }
+}
